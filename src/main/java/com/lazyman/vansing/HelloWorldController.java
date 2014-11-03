f7714989edd6bd78889556639862bdc7a4c6ff0d
cxqpfan <cxqpfan@163.com>
2014-11-03 20:06:52 +0800

    init

---
+++ src/main/java/com/lazyman/vansing/HelloWorldController.java
@@ -0,0 +1,20 @@
+package com.lazyman.vansing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * User: APPLE
+ * Date: 10/30/14
+ */
+@Controller
+public class HelloWorldController {
+
+
+
+    @RequestMapping("/hello.do")
+    public String hello()
+    {
+        return "hello";
+    }
+}

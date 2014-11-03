ff0e0c6129be7529e532ffbadc78f9234e8a4daa
cxqpfan <cxqpfan@163.com>
2014-11-03 22:30:47 +0800

    add hibernate

--- src/main/java/com/lazyman/vansing/HelloWorldController.java
+++ src/main/java/com/lazyman/vansing/HelloWorldController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorldController {
 
-
-
-    @RequestMapping("/hello.do")
+    @RequestMapping("/hello")
     public String hello()
     {
         return "hello";
